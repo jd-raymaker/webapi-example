f9e7bdf11da0dd35b0b9764d6197b64af2f40819
JayDee Raymaker <16980904+jd-raymaker@users.noreply.github.com>
2022-07-09 17:52:06 +0200

    Update to use DbContext with examples

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-WebApplication app = builder.Build();
+using var db = new ItemContext();
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Add some dummy data to the database
+db.Add(new Item {Name = "Tralala 1", Value = "Some value"});
+db.Add(new Item {Name = "Yet another item", Value = "Some more value here"});
+db.SaveChanges();
 
 // ==================
 // Endpoints
 
-app.MapGet( "/hello", () => "Hello World" );
+// Basic one-liner examples
+app.MapGet( "/", () => "Basic API with database example" );
 app.MapGet( "/time", () => DateTime.UtcNow );
+
+// List all items in database
+app.MapGet("/items", () =>
+    db.Items
+    .OrderBy(i => i.Id)
+);
+
+// How to Modify / Delete item number 2 using GET method example
+/*
+app.MapGet("/delete2", () =>
+{
+    // querry the item using Where method
+    var item = db.Items.Where(i => i.Id == 2).First();
+    db.Remove(item);
+    db.SaveChanges();
+});
+*/
 
 // ==================
 
