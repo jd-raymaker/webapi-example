686f6eb3433a95f3ae8fa14e329607afa4bab906
JayDee Raymaker <16980904+jd-raymaker@users.noreply.github.com>
2022-07-16 14:03:53 +0200

    Add comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+// Initialize new instance of WebAppBuilder
 var builder = WebApplication.CreateBuilder(args);
+// Build WebApp
 var app = builder.Build();
 
 ItemContext db = new ItemContext("temp-webapi");
@@ -16,6 +18,8 @@ app.MapGet("/items", () =>
     .OrderBy(i => i.Id)
 );
 
+// Receive POST request and store it to the database.
+// Accepts RAW packet with JSON
 app.MapPost("/new", async (Item i) =>
 {
     db.Add(i);
